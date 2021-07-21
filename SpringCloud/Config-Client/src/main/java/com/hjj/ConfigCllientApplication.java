c905c1489a38eadfb59ac9e9523b1c2bc9150686
liaochenpeng <243150929@qq.com>
2021-07-21 23:29:19 +0800

    add configClient
    add configClient

---
+++ SpringCloud/Config-Client/src/main/java/com/hjj/ConfigCllientApplication.java
@@ -0,0 +1,11 @@
+package com.hjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCllientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCllientApplication.class, args);
+    }
+}
